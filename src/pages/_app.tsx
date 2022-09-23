817212f973e0c7aa1d164ec37e89a9dd91c19d80
Hendra Wardana <maxnara19@gmail.com>
2022-09-23 18:32:41 +0700

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,13 @@ import { AppProps } from "next/app";
 // NOTE: Do not move the styles dir to the src.
 // They are used by the Netlify CMS preview feature.
 import "../../public/styles/global.css";
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7770880240485751" crossorigin="anonymous"></Script>
+      <Component {...pageProps} />;
+    </>
+  )
 }
